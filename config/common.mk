f9a82b02b6846d73c7e2fc90759625ef6faed70b
Oliver Scott <olivercscott@gmail.com>
2022-10-11 20:39:01 +0200

    config: Build BatteryStatsViewer
    Change-Id: I1f712533f1914f7155f7a6657e3fa2095b4ea83a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -174,6 +174,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
 
+# BatteryStatsViewer
+PRODUCT_PACKAGES += \
+    BatteryStatsViewer
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
