53489b3514dd510f85607f6661a4664d0e66f491
Vaisakh Murali <mvaisakh@statixos.com>
2023-12-22 23:33:02 +0530

    build: Introduce flower-release task
    This will build otapackage and updatepackage with the same command
    and generate ota json
    Lesser hassle, easier release.
    
    based on https://github.com/StatiXOS/android_vendor_statix/commit/37b171c7c643471d86c95ccbfedf6b3ec34dc29c
    
    Change-Id: I87476601dc42283e67b9a2bbd22c11ba033f7eef
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

---
+++ build/tasks/flower-release.mk
@@ -0,0 +1,49 @@
+# Copyright (C) 2017 Unlegacy-Android
+# Copyright (C) 2017,2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# -----------------------------------------------------------------
+# LumineDroid OTA update package
+
+LUMINE_TARGET_PACKAGE := $(PRODUCT_OUT)/LumineDroid-$(LUMINE_VERSION).zip
+LUMINE_TARGET_UPDATEPACKAGE := $(PRODUCT_OUT)/LumineDroid-$(LUMINE_VERSION)-img.zip
+LUMINE_BUILD_TIME := 
+
+MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
+
+.PHONY: flower-release
+flower-release: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET) $(INTERNAL_UPDATE_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LUMINE_TARGET_PACKAGE)
+	$(hide) $(MD5) $(LUMINE_TARGET_PACKAGE) > $(LUMINE_TARGET_PACKAGE).md5sum
+	$(hide) source ./vendor/lumine/build/tools/generate_json_build_info.sh $(LUMINE_TARGET_PACKAGE)
+	$(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(LUMINE_TARGET_UPDATEPACKAGE)
+	$(hide) $(MD5) $(LUMINE_TARGET_UPDATEPACKAGE) > $(LUMINE_TARGET_UPDATEPACKAGE).md5sum
+	@echo -e ${C								                         "${CL_BLU}
+	@echo -e ${CL_BLU}"                                                                              "${CL_BLU}
+	@echo -e ${CL_CYN}"=============================-OTA Package Details-============================"${CL_RST}
+	@echo -e ${CL_CYN}"OutputZip      : "${CL_MAG} $(LUMINE_TARGET_PACKAGE)${CL_RST}
+	@echo -e ${CL_CYN}"MD5            : "${CL_MAG}" $(shell cat $(LUMINE_TARGET_PACKAGE).md5sum | awk '{print $$1}')"${CL_RST}
+	@echo -e ${CL_CYN}"Size           : "${CL_MAG}" $(shell du -hs $(LUMINE_TARGET_PACKAGE) | awk '{print $$1}')"${CL_RST}
+	@echo -e ${CL_CYN}"Size(in bytes) : "${CL_MAG}" $(shell wc -c $(LUMINE_TARGET_PACKAGE) | awk '{print $$1}')"${CL_RST}
+	@echo -e ${CL_CYN}"Build Type     : "${CL_MAG} $(LUMINE_BUILD_TYPE)${CL_RST}
+	@echo -e ${CL_CYN}"==========================================================================="${CL_RST}
+	@echo -e ""
+	@echo -e ${CL_CYN}"============================-Fastboot Package Details-=============================="${CL_RST}
+	@echo -e ${CL_CYN}"OutputZip      : "${CL_MAG} $(LUMINE_TARGET_UPDATEPACKAGE)${CL_RST}
+	@echo -e ${CL_CYN}"MD5            : "${CL_MAG}" $(shell cat $(LUMINE_TARGET_UPDATEPACKAGE).md5sum | awk '{print $$1}')"${CL_RST} 
+	@echo -e ${CL_CYN}"Size           : "${CL_MAG}" $(shell du -hs $(LUMINE_TARGET_UPDATEPACKAGE) | awk '{print $$1}')"${CL_RST}
+	@echo -e ${CL_CYN}"Size(in bytes) : "${CL_MAG}" $(shell wc -c $(LUMINE_TARGET_UPDATEPACKAGE) | awk '{print $$1}')"${CL_RST} 
+	@echo -e ${CL_CYN}"Build Type     : "${CL_MAG} $(LUMINE_BUILD_TYPE)${CL_RST}
+	@echo -e ${CL_CYN}"==========================================================================="${CL_RST}
+	@echo -e ""
