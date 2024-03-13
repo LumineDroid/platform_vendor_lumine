20833b199c9aef3ccab1b3472c0504a861992cad
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-13 13:35:09 +0200

    version: Sign every build variant but eng
    Change-Id: Iad382aa2b2242b89014f1d659fdb4e764d0cca67

--- config/version.mk
+++ config/version.mk
@@ -31,7 +31,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     org.lumine.version=$(LINEAGE_VERSION)
 
 # Signing
-ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/lineage/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
