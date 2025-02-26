2005d343b0f47c5d9dc6bc510503784408a66470
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-26 21:37:22 +0800

    common: Disable art debug builds
    Change-Id: I44e16c12c70e30dea8e85a9751cc94e4ff329898
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -100,6 +100,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
+ART_BUILD_TARGET_NDEBUG := false
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := false
+ART_BUILD_HOST_DEBUG := false
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
@@ -112,6 +117,9 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # but also allow explicit overriding for testing and development.
 SYSTEM_OPTIMIZE_JAVA ?= true
 SYSTEMUI_OPTIMIZE_JAVA ?= true
+
+# Disable dex2oat debug
+USE_DEX2OAT_DEBUG := false
 
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
