38382b40a34582ec61ee21ec83030b1c992ee7f6
Michael Bestas <mkbestas@lineageos.org>
2025-04-24 23:28:44 +0300

    luminedroid: config: Move bootloader message offset to select()
    Change-Id: Ic97af638005c299963d0aaa39e04227dcbca8751

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -29,20 +29,17 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += lumineGlobalVars
 SOONG_CONFIG_lumineGlobalVars += \
     additional_gralloc_10_usage_bits \
-    bootloader_message_offset \
     camera_override_format_from_reserved
 
 # Soong bool variables
 SOONG_CONFIG_lumineGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
 
 # Set default values
-BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
 
 # Soong value variables
 SOONG_CONFIG_lumineGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
-SOONG_CONFIG_lumineGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)
@@ -90,6 +87,11 @@ ifneq ($(TARGET_TRUST_USB_CONTROL_ENABLE),)
 endif
 ifneq ($(TARGET_TRUST_USB_CONTROL_DISABLE),)
     $(call soong_config_set,lineage_usb,usb_control_disabled,$(TARGET_TRUST_USB_CONTROL_DISABLE))
+endif
+
+# Recovery
+ifneq ($(BOOTLOADER_MESSAGE_OFFSET),)
+    $(call soong_config_set,lineage_recovery,bootloader_message_offset,$(BOOTLOADER_MESSAGE_OFFSET))
 endif
 
 # Surfaceflinger
