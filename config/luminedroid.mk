f8316466a8bb1309923c6c062df0ee592cfc9b06
techyminati <sinha.aryan03@gmail.com>
2025-06-14 23:30:42 +0800

    config: Enable Material Design 3 Expressive
    Change-Id: Ib6d3ac2e0f3d76768c508e1b00efc51e141c1b09
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- config/luminedroid.mk
+++ config/luminedroid.mk
@@ -11,3 +11,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.sysui.disableBlur=true \
     ro.surface_flinger.supports_background_blur=0
 endif
+
+# Enable Material Design 3 Expressive
+PRODUCT_PRODUCT_PROPERTIES += \
+    is_expressive_design_enabled=true
