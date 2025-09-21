ca5a7297f918ac0d94a63f33b982aa7fba092ade
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-21 21:08:56 +0800

    config: adding flashlight strength flag
    Change-Id: I69f051676d6e3048307c48aead46a1b04d7b872d

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -33,6 +33,11 @@ endif
 PRODUCT_PACKAGES += \
     Launcher3Overlay
 
+TORCH_STR_SUPPORTED ?= true
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.torch_str_support=$(TORCH_STR_SUPPORTED)
+
 # Extra cmdline tools
 PRODUCT_PACKAGES += \
     unrar \
