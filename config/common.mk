cincaiAndroid/android_vendor_lineage

Reapply "lineage: Add framework compatibility matrix for Lineage HALs"
It's time to fix your devices.

This reverts commit 9887e1f75199cb37af46f13b53b3b5a8464d8466.

Change-Id: I78da6340f38b93fdc4c3694cb8f431f387d16c0d

config/common.mk
@@ -270,6 +270,8 @@ CUSTOM_LOCALES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/lineage/build/target/product/security/lineage
 

LumineDroid/platform_vendor_lumine

config/common.mk
@@ -247,6 +247,8 @@ PRODUCT_PACKAGES += \
     NetworkStackOverlay \
     PermissionControllerOverlay
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lumine/config/device_framework_matrix.xml
+
 # Translations
 CUSTOM_LOCALES += \
     ast_ES \