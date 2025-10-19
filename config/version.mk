c4a6798400c754bf44ceb1557ac52c8376b7d205
Hexaboo <hexaboo@outlook.com>
2025-10-19 05:16:55 +0000

    config: Redo versioning
    Change-Id: I4d378115a73f82ee515c203bc2ffdabecfd46fbe
    Signed-off-by: Hexaboo <hexaboo@outlook.com>

--- config/version.mk
+++ config/version.mk
@@ -1,4 +1,9 @@
-PRODUCT_VERSION := bynx
+LUMINE_BASE_VERSION := bynx
+LUMINE_BUILD_TYPE ?= UNOFFICIAL
+
+ifndef LUMINE_MAINTAINER
+LUMINE_MAINTAINER := Luminers
+endif
 
 ifeq ($(LINEAGE_VERSION_APPEND_TIME_OF_DAY),true)
     LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d_%H%M%S)
@@ -6,19 +11,23 @@ else
     LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d)
 endif
 
-LINEAGE_BUILDTYPE ?= UNOFFICIAL
-
-LINEAGE_VERSION_SUFFIX := $(LINEAGE_BUILD_DATE)-$(LINEAGE_BUILDTYPE)-$(LINEAGE_BUILD)
+LINEAGE_VERSION_SUFFIX := $(LINEAGE_BUILD_DATE)-$(LUMINE_BUILD_TYPE)-$(LINEAGE_BUILD)
 
 # Internal version
-LINEAGE_VERSION := $(PRODUCT_VERSION)-$(LINEAGE_VERSION_SUFFIX)
+LINEAGE_VERSION := $(LUMINE_BASE_VERSION)-$(LINEAGE_VERSION_SUFFIX)
+LUMINE_VERSION := LumineDroid-$(LUMINE_BASE_VERSION)-$(LINEAGE_VERSION_SUFFIX)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := $(PRODUCT_VERSION)-$(LINEAGE_VERSION_SUFFIX)
+LINEAGE_DISPLAY_VERSION := $(LUMINE_BASE_VERSION)-$(LINEAGE_VERSION_SUFFIX)
 
 # LumineDroid version properties
 PRODUCT_SYSTEM_PROPERTIES += \
+    org.luminedroid.version=$(LUMINE_BASE_VERSION) \
+    org.luminedroid.build.type=$(LUMINE_BUILD_TYPE) \
+    org.luminedroid.maintainer=$(LUMINE_MAINTAINER)
+
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.lineage.version=$(LINEAGE_VERSION) \
     ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.lineage.build.version=$(PRODUCT_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE)
+    ro.lineage.build.version=$(LUMINE_BASE_VERSION) \
+    ro.lineage.releasetype=$(LUMINE_BUILD_TYPE)
