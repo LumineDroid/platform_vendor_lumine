049e832ad914513c7d967e059196c64dbbf959f3
hridaya <hridaya@pixelos.net>
2025-12-07 09:50:52 +0545

    config: Drop SimpleSettingsConfig
    Change-Id: I2235ad147a3c36b4e399c61a2adf1909b428a944

--- config/common.mk
+++ config/common.mk
@@ -121,10 +121,6 @@ endif
 PRODUCT_PACKAGES += \
     framework_compatibility_matrix.lineage.xml
 
-# Config
-PRODUCT_PACKAGES += \
-    SimpleSettingsConfig
-
 # Extra tools
 PRODUCT_PACKAGES += \
     bash \
