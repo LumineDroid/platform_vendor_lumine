72619df668c5e3b24c22a68440eb0d4c034ee989
Hexaboo <hexaboo@outlook.com>
2026-01-07 16:52:54 +0100

    vendor: Rebrand to LumineDroid
    Change-Id: I641fa9f33e8cbe3ceaf95e4744634da850ef21d4
    Signed-off-by: Hexaboo <hexaboo@outlook.com>

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func lineageExpandVariables(ctx android.ModuleContext, in string) string {
-	lineageVars := ctx.Config().VendorConfig("lineageVarsPlugin")
+func lumineExpandVariables(ctx android.ModuleContext, in string) string {
+	lumineVars := ctx.Config().VendorConfig("lumineVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if lineageVars.IsSet(name) {
-			return lineageVars.String(name), nil
+		if lumineVars.IsSet(name) {
+			return lumineVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,12 +1,9 @@
-# Inherit common mobile Lineage stuff
-$(call inherit-product, vendor/lineage/config/common.mk)
+# Inherit common mobile LumineDroid stuff
+$(call inherit-product, vendor/lumine/config/common.mk)
 
 # Include AOSP audio files
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-include vendor/lineage/config/aosp_audio.mk
-
-# Include Lineage audio files
-include vendor/lineage/config/lineage_audio.mk
+include vendor/lumine/config/aosp_audio.mk
 
 # Default notification/alarm sounds
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -14,11 +11,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.alarm_alert=Hassium.ogg
 
 # Apps
+ifneq ($(PRODUCT_NO_CAMERA),true)
 PRODUCT_PACKAGES += \
-    AvatarPicker \
-    Backgrounds \
-    Glimpse \
-    LatinIME
+    Aperture
+endif
+
+ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
+PRODUCT_PACKAGES += \
+    AudioFX
+endif
 
 ifeq ($(PRODUCT_TYPE), go)
 PRODUCT_PACKAGES += \
@@ -37,19 +38,10 @@ endif
 PRODUCT_PACKAGES += \
     Launcher3Overlay
 
-# Charger
+# Extra cmdline tools
 PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-PRODUCT_PACKAGES += \
-    lineage_charger_animation \
-    lineage_charger_animation_vendor
-endif
-
-# Legal
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    unrar \
+    zstd
 
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -74,6 +66,5 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 
 # Themes
 PRODUCT_PACKAGES += \
-    LineageBlackTheme \
     ThemePicker \
     ThemesStub
