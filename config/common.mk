bananadroid/android_vendor_banana

vendor: Add preinstalled package list to stop auto cloning
Only extra adds needed, gms now ships it's own lists for this.

config/common.mk
@@ -67,6 +67,10 @@ PRODUCT_COPY_FILES += \
     vendor/banana/prebuilt/common/etc/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml \
     vendor/banana/prebuilt/common/etc/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-custom.xml
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/banana/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-custom-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-custom-product.xml
+
 # Include Banana extra packages
 include vendor/banana/config/packages.mk
 include vendor/banana/config/prop.mk

LumineDroid/platform_vendor_lumine

Change-Id: I0917a763ef781471d1a63262de268a916b846f89
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

config/common.mk
@@ -71,6 +71,10 @@ endif
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lumine/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-lumine-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-lumine-product.xml
+
 # Credential storage
 PRODUCT_PACKAGES += \
     android.software.credentials.prebuilt.xml